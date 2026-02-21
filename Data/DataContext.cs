e92226b70432f586f85cecbd8acd9bb8f034ce41
Semeniuk Oleg <semeniuk.oleg2002@gmail.com>
2026-02-21 19:46:29 +0200

    Add project files.

---
+++ Data/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using EFP48.Data.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFP48.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"Data Source=(LocalDB)\MSSQLLocalDB;
+                Initial Catalog=efp48;
+                Integrated Security=True")
+                .LogTo(Console.WriteLine, new[] { Microsoft.EntityFrameworkCore.Diagnostics.RelationalEventId.CommandExecuted })
+                .EnableSensitiveDataLogging();
+        }
+    }
+}
---
+++ Data/Entity/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFP48.Data.Entity
+{
+    public class Product
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public double Price { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime? DeletedAt { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID: {Id} - Name: {Name}";
+        }
+    }
+}
