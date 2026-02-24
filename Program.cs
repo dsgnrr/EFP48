e4b102b6f0dc3b5975dedb1589528f738c71431b
Semeniuk Oleg <semeniuk.oleg2002@gmail.com>
2026-02-24 18:21:54 +0200

    edit program.cs

--- Program.cs
+++ Program.cs
@@ -5,26 +5,98 @@ namespace EFP48
 {
     public class Program
     {
+        public static int ShowProductCrudMenu()
+        {
+            Console.WriteLine(@"
+1. Get all products
+2. Get product by id
+3. Create new product
+4. Update product
+5. Delete product
+6. Exit
+");
+            int result;
+            while (!int.TryParse(Console.ReadLine(), out result)) {
+                Console.WriteLine("Enter number 1-6:");
+              }
+            return result;
+        }
+
+
+        public static void printEntity<T>(List<T> values, string entityName)
+        {
+            if (values.Count != 0)
+            {
+                Console.WriteLine($"-------------- ${entityName} --------------");
+                foreach (var item in values)
+                {
+                    Console.WriteLine("\n");
+                    Console.WriteLine(item);
+                    Console.WriteLine("\n");
+                }
+            }
+            else Console.WriteLine($"No: {entityName}");
+        }
+
+        public static List<Product> getAllProducts(DataContext? dataContext, bool showDeleted=false)
+        {
+            if(dataContext== null)
+            {
+                Console.WriteLine("Error: Data context not provided");
+                throw new Exception("Data Context not provided");
+            }
+            var products = dataContext.Products.AsQueryable();
+
+            if (!showDeleted) products = products.Where(p => p.DeletedAt == null);
+
+            return products.ToList();
+        }
+
         public static void Main(string[] args)
         {
-            // Створюємо єкземпляр ДатаКонтексту
-            // За допомогою дата-контексту у нас є можливість надсилати запити до БД
+
+            // CRUD - Create, Read, Update, Delete
+
             DataContext dataContext = new();
 
-            // Створення нового продутку
-            //var product = new Product
-            //{
-            //    Id = Guid.NewGuid(),
-            //    Name = "Samsung S26 Ultra",
-            //    Price = 1500,
-            //    CreatedAt = DateTime.Now,
-            //};
 
-            
+            bool isExit = false;
+            while (!isExit)
+            {
+                Console.Clear();
+                int menuItem = ShowProductCrudMenu();
+                switch (menuItem)
+                {
+                    case 1:
+                        var products = getAllProducts(dataContext);
+                        printEntity<Product>(products, "Products");
+                        break;
+
+                }
+
+            }
+            #region Lesson1
+            /*
+            DataContext dataContext = new();
+            ShowProductCrudMenu();
+            // Створюємо єкземпляр ДатаКонтексту
+            // За допомогою дата-контексту у нас є можливість надсилати запити до БД
+
+
+            // Створення нового продутку
+            var new_product = new Product
+            {
+                Id = Guid.NewGuid(),
+                Name = "Sony PlayStation 5 Pro",
+                Price = 1500,
+                CreatedAt = DateTime.Now,
+            };
+
+
             // Додаємо продукт у DbSet
-            //dataContext.Products.Add(product);
+            dataContext.Products.Add(new_product);
             // Оновлюємо дані таблиці, зберігаємо зміни
-            //dataContext.SaveChanges();
+            dataContext.SaveChanges();
 
 
             // Отримання всіх продуктів
@@ -69,6 +141,8 @@ namespace EFP48
                 dataContext.SaveChanges();
             }
 
+            */
+            #endregion
 
         }
     }
