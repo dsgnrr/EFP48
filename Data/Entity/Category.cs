ad1c2dfe310361fd9c374606cb8ab1adb76d1fb5
Semeniuk Oleg <semeniuk.oleg2002@gmail.com>
2026-03-03 19:34:25 +0200

    Seed

--- Data/Entity/Category.cs
+++ Data/Entity/Category.cs
@@ -15,7 +15,7 @@ namespace EFP48.Data.Entity
         [Column(TypeName = "varchar(MAX)")]
         public string? Description { get; set; }
 
-        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime CreatedAt { get; set; }
         public DateTime? DeletedAt { get; set; }
 
         // navigation props
--- Migrations/20260303165557_DataSeed.Designer.cs
+++ Migrations/20260303165557_DataSeed.Designer.cs
@@ -4,6 +4,7 @@ using EFP48.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace EFP48.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20260303165557_DataSeed")]
+    partial class DataSeed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
